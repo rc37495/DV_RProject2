ee8b1825feb12be5545a22ee9e46b221819803df
Raymundo Cortez <rc37495@utexas.edu>
2015-10-03 02:11:08 -0500

    final

--- 03 Visualizations/workflow1.R
+++ 03 Visualizations/workflow1.R
@@ -1,4 +1,5 @@
 require(dplyr)
 require(ggplot2)
+
 ndf %>% select(TOWN_CITY,DISTRICT,PRICE,OLD_NEW) %>% filter(DISTRICT == "LEEDS") %>% arrange(OLD_NEW) %>% ggplot(aes(x = TOWN_CITY, y = PRICE, colour = OLD_NEW)) + geom_bar(stat="identity") + labs(x = "Town/City",y = "Price",title="Leeds District") + theme(axis.text.x=element_text(angle=50,color="black"))
 
